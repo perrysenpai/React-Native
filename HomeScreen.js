703757a90f28c6768f3421825f5cf81727548885
perrysenpai <46563743+perrysenpai@users.noreply.github.com>
2019-11-11 15:28:23 +0530

    Navigation- Buttons and TouchableOpacity

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet , View, Button, TouchableOpacity} from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = (props) => {     {/* we dont need to use props we can dorectly use navigation here */}
   return (
   	<View>
  	 	<Text style={styles.text}>Tut comms</Text>
  	 	<Button 
- 	 		onPress={()=> props.navigation.navigate("Components")}
+ 	 		onPress={()=> props.navigation.navigate("Components")}   {/* we dont need to use props we can directly use navigation here */}
  	 		title="Go to Components Demo"/>
 
- 	 	<TouchableOpacity  onPress={()=> props.navigation.navigate("Another")}>
+ 	 	<TouchableOpacity  onPress={()=> props.navigation.navigate("Another")}>   
  	 	 <Text>Go to List Demo</Text>
  	 	</TouchableOpacity>
 
